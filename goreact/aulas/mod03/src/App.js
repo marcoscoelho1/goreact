d1406c477e40845cbcd6a0e8760a561e4d1d933f
Marcos Coelho <marcoscoelho1@yahoo.com.br>
2019-07-02 00:11:34 -0300

    iniciando estudos de react + redux

---
+++ goreact/aulas/mod03/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+    </Provider>
+  );
+}
+
+export default App;

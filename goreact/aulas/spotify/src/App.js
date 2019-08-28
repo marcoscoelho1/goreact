31e5e385b21219a346b6fed5ee0e237dd482445e
Marcos Coelho <marcoscoelho1@yahoo.com.br>
2019-08-28 00:30:25 -0300

    aulas de css-grid

--- goreact/aulas/spotify/src/App.js
+++ goreact/aulas/spotify/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import GlobalStyle from './styles/global';
-import Sidebar from './components/Sidebar';
+import React from "react";
+import GlobalStyle from "./styles/global";
+import Sidebar from "./components/Sidebar";
 
 const App = () => (
   <>
